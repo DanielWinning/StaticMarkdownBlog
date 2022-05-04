ccccdee8adec624988e161c91c29166acafa73e9
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-05-05 00:44:12 +0100

    added illuminate/support + created service provider

---
+++ src/StaticMarkdownBlogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace DanielWinning\StaticMarkdownBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class StaticMarkdownBlogServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit0827792e2024c174b4b5a6648d4052ac
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit0827792e2024c174b4b5a6648d4052ac', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit0827792e2024c174b4b5a6648d4052ac', 'loadClassLoader'));
@@ -50,6 +52,29 @@ class ComposerAutoloaderInit0827792e2024c174b4b5a6648d4052ac
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit0827792e2024c174b4b5a6648d4052ac::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire0827792e2024c174b4b5a6648d4052ac($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire0827792e2024c174b4b5a6648d4052ac($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
