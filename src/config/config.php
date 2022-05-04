06c1dcc3f815010c1556054bc08041ee8ae445d5
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-05-05 00:51:43 +0100

    added config file

---
+++ src/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /**
+     * Package information
+     */
+    "name" => "Static Markdown Blog",
+    "slug" => "static-markdown-blog",
+    "version" => "1.0.0",
+
+    /**
+     * Where the markdown posts are stored
+     */
+    "postsPath" => base_path("resources/posts"),
+
+    /**
+     * The URL prefix for your applications blog posts
+     */
+    "postsUrl" => "/blog"
+];
