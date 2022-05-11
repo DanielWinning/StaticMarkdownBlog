954dd07064a77fe7010180192545568c3374a667
Daniel Winning <daniel@winningwebdesign.co.uk>
2022-05-11 13:00:02 +0100

    added classes to pagination links for easier styling

--- src/resources/views/posts/paginator.blade.php
+++ src/resources/views/posts/paginator.blade.php
@@ -5,12 +5,14 @@
         </div>
         <div class="smb-pagination-links-buttons">
             @if($paginator->currentPage > 1)
-                <a href="{{ config("static-markdown-blog.postsUrl") . "?page=" . $paginator->currentPage - 1 }}">
+                <a href="{{ config("static-markdown-blog.postsUrl") . "?page=" . $paginator->currentPage - 1 }}"
+                   class="smb-pagination-previous-link">
                     Previous
                 </a>
             @endif
             @if($paginator->currentPage < $paginator->pageCount)
-                <a href="{{ config("static-markdown-blog.postsUrl") . "?page=" . $paginator->currentPage + 1 }}">
+                <a href="{{ config("static-markdown-blog.postsUrl") . "?page=" . $paginator->currentPage + 1 }}"
+                   class="smb-pagination-next-link">
                     Next
                 </a>
             @endif
